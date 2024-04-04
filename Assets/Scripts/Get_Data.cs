e0b7a28e1f7a4757716910356e1d49267316358e
Otterly-Cute <144897039+Otterly-Cute@users.noreply.github.com>
2024-04-04 21:12:06 +0200

    copied script

---
+++ Assets/Scripts/Get_Data.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Get_Data : MonoBehaviour
+{
+    public TextMeshProUGUI accelerometerText;
+    public TextMeshProUGUI dataPointsCollected;
+    public int number;
+    public Array dataPoints = new int[arraySize];
+    private static int arraySize = 200;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //check if we have a accelerometer
+        if (SystemInfo.supportsAccelerometer)
+        {
+            accelerometerText.text = "Accelerometer Supported";
+        }
+        else
+        {
+            accelerometerText.text = "Accelerometer Not Supported";
+        }
+        number = 0;
+        dataPointsCollected.text = "Nr." + number;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void NumberOfDataPoints()
+    {
+        number += 1;
+        dataPointsCollected.text = "Nr." + number;
+    }
+
+    public void AddDataPointToArray()
+    {
+
+    }
+}
